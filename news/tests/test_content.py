1eceecfebb716bc8f28e092dbce9b326cfdeb3d5
Sainekt <xcision@ya.ru>
2024-05-22 17:28:21 +0300

    test content done

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timedelta
 
+from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
-from news.models import News
+from news.models import News, Comment
+from news.forms import CommentForm
+
+User = get_user_model()
 
 class TestContent(TestCase):
     
@@ -33,3 +38,39 @@ class TestContent(TestCase):
         all_dates = [news.date for news in object_list]
         sorted_dates = sorted(all_dates, reverse=True)
         self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая запись', text='Просто текст.')
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        now = timezone.now()
+
+        for index in range(10):
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Текст {index}')
+            comment.created = now + timedelta(days=index)
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        self.assertIn('news', response.context)
+        news = response.context['news']
+        all_comments = news.comment_set.all()
+        all_time_stamps = [comment.created for comment in all_comments]
+        sorted_timestamps = sorted(all_time_stamps)
+        self.assertCountEqual(sorted_timestamps, all_time_stamps)
+
+    def test_anonymous_client_has_no_form(self):
+        response = self.client.get(self.detail_url)
+        self.assertNotIn('form', response.context)
+
+    def test_authorized_client_has_form(self):
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], CommentForm)
